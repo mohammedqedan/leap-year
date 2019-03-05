86ad2c72dbff460525c7db66912694c3d16a417d
Student <example@example.com>
2019-03-04 19:39:59 -0800

    leap year and prime numbers

---
+++ js/prime.js
@@ -0,0 +1,45 @@
+function isPrime(num) {
+
+  if (num === 2) {
+    return true;
+  }
+  else if(num > 1){
+    for (var i = 2;  i < num; i++) {
+
+      if (num % i !== 0 ) {
+        return true;
+      }
+
+      else if (num === i * i) {
+        return false
+      }
+
+      else {
+        return false;
+      }
+    }
+  }
+  else {
+    return false;
+  }
+
+}
+console.log(isPrime(121));
+// function
+$(document).ready(function() {
+  $("form#prime_number").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var result = isPrime(number);
+
+    $(".prime").text(number);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
